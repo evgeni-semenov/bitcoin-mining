3d8f67febfce038742534a7051984ddf009798e8
Evgeni Semenov <evgeni.semenov@gmail.com>
2022-10-07 13:32:28 +0300

    First commit

--- bitcoin-mining.py
+++ bitcoin-mining.py
@@ -2,12 +2,14 @@
 '''
 This is a bitcoin mining process simulator.
 
-The process is of course oversimplified, but you will get a general idea. Normally each bitcoin block includes:
+This simulation is, of course, oversimplified, but you will get a general idea. 
+Normally each bitcoin block includes:
 - a hash value of previous block (to verify integrity of the blockchain and all the previous transactions)
 - transactions included in the block
 - a random numeric nonce value
 
-Miners form the blocks and try different nonce values to solve the hash puzzle = 
+Miners form the blocks and try different nonce values to solve the hash puzzle = find a hash value that matches the criteria.
+In Bitcoin network this criteria is simply a N amount of prefix zeros. 
 
 You can experiment with adjusting network_difficulty (although I don't recommend to go above 7-8), and modifying the 
 transactions to see how it affects the mining time.
